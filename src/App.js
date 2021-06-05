b1857f114e3347ad6d87f2f10fe7ac471839b6f9
Adesh <61568773+adeshrai6@users.noreply.github.com>
2021-06-04 23:22:20 -0400

    staging app files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Header from "./components/Header";
+import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  );
-}
+const App = () => {
+  return <div>E-Commerce</div>;
+};
 
 export default App;
